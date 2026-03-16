353df82322c4e7c05a3f379c3fe25f6a4d072cf5
Elizaveta <el1zaveta-vladimirova@bk.ru>
2026-03-16 16:47:40 +0300

    Добавить конфигурацию приложения и вынести настройки из Program.cs.

---
+++ Tailly.AuthService/Configurations/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,162 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Tailly.AuthService.Configurations.Options;
+using Tailly.AuthService.DataAccess;
+using Tailly.AuthService.Repositories;
+using Tailly.AuthService.Repositories.Interfaces;
+using Tailly.AuthService.Service.Auth;
+using Tailly.AuthService.Service.Security;
+using Tailly.AuthService.Service.Tokens;
+
+namespace Tailly.AuthService.Configurations.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddConfiguration(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddControllers();
+
+        services.AddSwaggerSetup();
+        services.AddPostgres(configuration);
+        services.AddOptions(configuration);
+        services.AddJwtAuthentication(configuration);
+
+        services.AddSecurity();
+        services.AddAuthenticationCore();
+
+        services.AddApplicationRepositories();
+        services.AddApplicationServices();
+
+        return services;
+    }
+
+    private static IServiceCollection AddPostgres(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AuthDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("Postgres")));
+
+        return services;
+    }
+
+    private static IServiceCollection AddOptions(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(
+            configuration.GetSection(nameof(JwtOptions)));
+
+        return services;
+    }
+
+    private static IServiceCollection AddJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var settings = configuration
+            .GetSection(nameof(JwtOptions))
+            .Get<JwtOptions>()!;
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(settings.Key));
+
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = settings.Issuer,
+
+                    ValidateAudience = true,
+                    ValidAudience = settings.Audience,
+
+                    ValidateLifetime = true,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key
+                };
+            });
+
+        return services;
+    }
+
+    private static IServiceCollection AddSecurity(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IPasswordHashingService, PasswordHashingService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthenticationCore(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+        services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddApplicationRepositories(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IUsersRepository, UsersRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddApplicationServices(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddSwaggerSetup(
+        this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition(
+                JwtBearerDefaults.AuthenticationScheme,
+                new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+
+            options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+        });
+
+        return services;
+    }
+}
