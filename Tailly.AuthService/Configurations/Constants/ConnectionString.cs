88f1b2b5400a4a9c1d55a0e30ba47becedce4817
Elizaveta <el1zaveta-vladimirova@bk.ru>
2026-03-17 19:48:19 +0300

    Добавить интеграцию Redis

---
+++ Tailly.AuthService/Configurations/Constants/ConnectionString.cs
@@ -0,0 +1,7 @@
+﻿namespace Tailly.AuthService.Configurations.Constants;
+
+public static class ConnectionStrings
+{
+    public const string Postgres = "Postgres";
+    public const string Redis = "Redis";
+}
